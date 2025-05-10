000c6c015a3b8a2e126c027f34b7f00fac9fd4c3
fumamosss <fumamosss@icloud.com>
2025-05-11 03:46:30 +0500

    refactor: change tab size

--- src/handlers/register-commands.js
+++ src/handlers/register-commands.js
@@ -18,40 +18,36 @@ const commandsFolder = resolve(__dirname, '../commands');
 const commandFiles = fs.readdirSync(commandsFolder);
 
 for (const commandFile of commandFiles) {
-    console.log(`Found command file: ${commandFile}!`);
+	console.log(`Found command file: ${commandFile}!`);
 
-    const commandPath = join(commandsFolder, commandFile)
-    const commandPathURL = pathToFileURL(commandPath);
-    try {
-        const command = await import(commandPathURL);
-        const mod = command.default ?? command;
-        if ('data' in mod && 'execute' in mod) {
-            commands.push(mod.data.toJSON());
-        } else {
-            console.log(`[WARNING] The command ${commandFile} at ${commandPath} is missing data or execute.`);
-        }
-    } catch (e) {
-        console.error(e)
-    }
+	const commandPath = join(commandsFolder, commandFile)
+	const commandPathURL = pathToFileURL(commandPath);
+	const command = await import(commandPathURL);
+	const mod = command.default ?? command;
+	if ('data' in mod && 'execute' in mod) {
+		commands.push(mod.data.toJSON());
+	} else {
+		console.log(`[WARNING] The command ${commandFile} at ${commandPath} is missing data or execute.`);
+	}
 }
 
 const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 (async () => {
-    try {
-        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-        if (COMMANDS_GLOBAL === 'true') {
-            console.log('Reloading globaly');
-            await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-        } else {
-            console.log('Reloading not globaly');
-            await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands },);
-        }
+		if (COMMANDS_GLOBAL === 'true') {
+			console.log('Reloading globaly');
+			await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+		} else {
+			console.log('Reloading not globaly');
+			await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands },);
+		}
 
-        console.log(`Successfully reloaded application (/) commands.`);
-    } catch (error) {
-        // And of course, make sure you catch and log any errors!
-        console.error(error);
-    }
+		console.log(`Successfully reloaded application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
 })();
